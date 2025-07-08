35e0fb6f622695f06a807da8259d811309f8fb7c
Jack3G <40290244+Jack3G@users.noreply.github.com>
2025-07-09 02:49:24 +1000

    update packages and handle mason-lspconfig breaking changes

--- init.lua
+++ init.lua
@@ -215,33 +215,29 @@ require("lazy").setup({
          require("mason-lspconfig").setup()
 
          local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-         require("mason-lspconfig").setup_handlers({
-            function(server_name)
-               require("lspconfig")[server_name].setup({
-                  capabilities = cmp_capabilities,
+         for _,server in pairs(require("mason-lspconfig").get_installed_servers()) do
+            require("lspconfig")[server].setup({
+               capabilities = cmp_capabilities,
+            })
+         end
+
+         require("lspconfig").lua_ls.setup({
+            capabilities = cmp_capabilities,
+            on_init = function(client)
+               local path = client.workspace_folders[1].name
+               if vim.loop.fs_stat(path.."/.luarc.json") or vim.loop.fs_stat(path.."/.luarc.jsonc") then
+                  return
+               end
+
+               client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                  runtime = { version = "LuaJIT" },
+                  workspace = {
+                     checkThirdParty = false,
+                     library = { vim.env.VIMRUNTIME },
+                  },
                })
             end,
-
-            ["lua_ls"] = function()
-               require("lspconfig").lua_ls.setup({
-                  capabilities = cmp_capabilities,
-                  on_init = function(client)
-                     local path = client.workspace_folders[1].name
-                     if vim.loop.fs_stat(path.."/.luarc.json") or vim.loop.fs_stat(path.."/.luarc.jsonc") then
-                        return
-                     end
-
-                     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                        runtime = { version = "LuaJIT" },
-                        workspace = {
-                           checkThirdParty = false,
-                           library = { vim.env.VIMRUNTIME },
-                        },
-                     })
-                  end,
-                  settings = { Lua = {} },
-               })
-            end,
+            settings = { Lua = {} },
          })
 
          vim.api.nvim_create_autocmd("LspAttach", {
