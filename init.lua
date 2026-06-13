4301bc3c49126476ddeb9fe0e816655fb075acd8
Jack3G <40290244+Jack3G@users.noreply.github.com>
2026-06-13 14:17:52 +1000

    limit maximum lines of context for nvim-treesitter-context

--- init.lua
+++ init.lua
@@ -156,9 +156,8 @@ require("lazy").setup({
 
    {
       "nvim-treesitter/nvim-treesitter-context",
-      dependencies = {
-         "nvim-treesitter/nvim-treesitter",
-      },
+      -- dependencies = { "nvim-treesitter/nvim-treesitter", },
+      opts = { max_lines = 5, },
    },
 
 
